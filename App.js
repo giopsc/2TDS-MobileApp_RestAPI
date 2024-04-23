012ddaa2e6864258c56e55a2c51b271eff8fb7cb
giopsc <logonrmlocal@fiap.com.br>
2024-04-23 09:44:31 -0300

    Aula sobre Rest API

--- App.js
+++ App.js
@@ -1,11 +1,34 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
+import { useState } from 'react';
+import Counter from "react-native-counters";
+import axios from 'axios';
 
 export default function App() {
+
+  const [ texto, setTexto ] = useState("");
+  const [ participants, setParticipants ] = useState(1);
+
+  const onPressButton = () => {
+    axios.get(`http://www.boredapi.com/api/activity?participants=${participants}`)
+    .then(response => {
+      console.log("resposta:", response.data.activity);
+      setTexto(response.data.activity);
+    })
+    .catch(e => {
+      console.log("error:", e);
+    })
+    .finally(() => {
+      console.log("Fim.");
+    })
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.texto}>{texto}</Text>
+      <StatusBar style="auto"/>
+      <Counter start={1} min={1} max={5} onChange={(number) => {setParticipants(number)}}/>
+      <Button title='Gerar nova atividade' onPress={onPressButton}></Button>
     </View>
   );
 }
@@ -15,6 +38,9 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'space-around',
   },
+  texto: {
+    fontSize: 20
+  }
 });
